6e492bc909845ad61543afa1d0eb44d21d79bbc8
Aznaur <aznaur.khapchaev@gmail.com>
2019-11-29 02:42:09 +0300

    слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -18,4 +18,47 @@
       }
     });
   }
+
+  var width = 300; // ширина картинки
+  var count = 4; // видимое количество изображений
+  var list = document.querySelector('.trainers__list');
+  var listElems = document.querySelectorAll('.trainers__item');
+
+  var position = 0; // положение ленты прокрутки
+
+  if (window.innerWidth <= 1024) {
+    count = 2;
+    document.querySelector('.trainers__arrow--prev').onclick = function () {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      list.style.marginLeft = position + 'px';
+    };
+
+    document.querySelector('.trainers__arrow--next').onclick = function () {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+  } else {
+    document.querySelector('.trainers__arrow--prev').onclick = function () {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      list.style.marginLeft = position + 'px';
+    };
+
+    document.querySelector('.trainers__arrow--next').onclick = function () {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+  }
+
 })();
