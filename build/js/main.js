4283bf50304cd20403073949c0ad6d7866573229
Aznaur <aznaur.khapchaev@gmail.com>
2019-12-04 23:38:17 +0300

    Правки аудит

---
+++ build/js/main.js
@@ -0,0 +1,121 @@
+'use strict';
+
+(function () {
+  var numberOfMonths = document.querySelectorAll('.duration-list__item');
+
+  for (var i = 0; i < numberOfMonths.length; i++) {
+    numberOfMonths[i].addEventListener('click', function (evt) {
+      var target = evt.target;
+
+      if (target.classList.contains('duration-list__item--active')) {
+        // target.classList.remove('duration-list__item--active');
+      } else {
+        var itemActive = document.querySelector('.duration-list__item--active');
+        if (itemActive) {
+          itemActive.classList.remove('duration-list__item--active');
+        }
+        target.classList.add('duration-list__item--active');
+      }
+    });
+  }
+
+  var marginRightItem = 40;
+  var width = document.querySelector('.trainers__item').offsetWidth + marginRightItem; // ширина картинки
+  var count = 4; // видимое количество изображений
+  var list = document.querySelector('.trainers__list');
+  var listElems = document.querySelectorAll('.trainers__item');
+
+  var position = 0; // положение ленты прокрутки
+
+  if (window.innerWidth < 1024) {
+    marginRightItem = 30;
+    width = document.querySelector('.trainers__item').offsetWidth + marginRightItem;
+    count = 2;
+    document.querySelector('.trainers__arrow--prev').onclick = function () {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      list.style.marginLeft = position + 'px';
+    };
+
+    document.querySelector('.trainers__arrow--next').onclick = function () {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+  }
+  if (window.innerWidth < 768) {
+    marginRightItem = 30;
+    width = 226 + marginRightItem;
+    count = 1;
+    document.querySelector('.trainers__arrow--prev').onclick = function () {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      list.style.marginLeft = position + 'px';
+    };
+
+    document.querySelector('.trainers__arrow--next').onclick = function () {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+  } else {
+    document.querySelector('.trainers__arrow--prev').onclick = function () {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0);
+      list.style.marginLeft = position + 'px';
+    };
+
+    document.querySelector('.trainers__arrow--next').onclick = function () {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
+  }
+
+  var reviewsWidth = 700;
+  var reviewsCount = 1;
+
+  var reviewsList = document.querySelector('.reviews__list ');
+  var reviewsListElems = document.querySelectorAll('.reviews__item');
+  var reviewsPosition = 0;
+
+  if (window.innerWidth < 768) {
+    reviewsWidth = 320;
+    document.querySelector('.reviews__arrow--prev').onclick = function () {
+      reviewsPosition += reviewsWidth * reviewsCount;
+      reviewsPosition = Math.min(reviewsPosition, 0);
+      reviewsList.style.marginLeft = reviewsPosition + 'px';
+    };
+
+    document.querySelector('.reviews__arrow--next').onclick = function () {
+      reviewsPosition -= reviewsWidth * reviewsCount;
+      reviewsPosition = Math.max(reviewsPosition, -reviewsWidth * (reviewsListElems.length - reviewsCount));
+      reviewsList.style.marginLeft = reviewsPosition + 'px';
+    };
+  } else {
+    document.querySelector('.reviews__arrow--prev').onclick = function () {
+      reviewsPosition += reviewsWidth * reviewsCount;
+      reviewsPosition = Math.min(reviewsPosition, 0);
+      reviewsList.style.marginLeft = reviewsPosition + 'px';
+    };
+
+    document.querySelector('.reviews__arrow--next').onclick = function () {
+      reviewsPosition -= reviewsWidth * reviewsCount;
+      reviewsPosition = Math.max(reviewsPosition, -reviewsWidth * (reviewsListElems.length - reviewsCount));
+      reviewsList.style.marginLeft = reviewsPosition + 'px';
+    };
+  }
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,17 @@ gulp.task("css", function () {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -39,7 +39,63 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 6}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+
+    .pipe(gulp.dest("source/img"));
+
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite_auto.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source//*.ico"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
